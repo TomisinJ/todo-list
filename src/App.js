702f23d6a15dc385d78fa2d35e29bc41458084ba
Tomisin <tomisinjoseph3@gmail.com>
2023-09-29 23:39:04 +0100

    Created reusable components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Todo from './components/Todo';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton';
 
-function App() {
+function App(props) {
+
+  const taskList = props.tasks?.map((task) => (
+    <Todo
+     id={task.id} 
+     name={task.name}
+      completed={task.completed} 
+      key={task.id}
+      />
+  ));
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className='todoapp stack-large'>
+      <h1>Day of Small Things List</h1>
+      <Form/>
+      <div className='filters btn-group stack-exception'>
+        <FilterButton/>
+        <FilterButton/>
+        <FilterButton/>
+      </div>
+      <h2 id='list-heading'>3 tasks remaining</h2>
+      <ul
+        role='list'
+        className='todo-list stack-large stack-exception'
+        aria-labelledby='list-heading'>
+          {taskList}
+      </ul>
+    </div>    
   );
 }
 
 export default App;
+
+// Instead of this, we have taskList:
+// <Todo name='eat' completed={true} id='todo-0'/>
+// <Todo name='sleep' completed={false} id='todo-1'/>
+// <Todo name='repeat' completed={false} id='todo-2'/>
