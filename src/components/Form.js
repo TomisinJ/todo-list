fc353892710bd65d59dfcf52262af39bceaf34c1
Tomisin <tomisinjoseph3@gmail.com>
2024-02-15 20:48:09 +0000

    Updated css

--- src/components/Form.js
+++ src/components/Form.js
@@ -19,7 +19,7 @@ function Form(props) {
     <form onSubmit={handleSubmit}>
     <h2 className='label-wrapper'>
       <label htmlFor='new-todo-input' className='label__lg'>
-        What it do?
+        What's on the list?
       </label>
     </h2>
     <input
